7e3ad48541437a93abb03bee85931da5f8cdb124
Rozul Imam <rozulimam@gmail.com>
2020-02-17 00:13:57 +0700

    update asset dir

---
+++ asset/js/custom.js
@@ -0,0 +1,28 @@
+$(document).on('click','#btn_search',function(e){
+	 swal("Perhatian","Mohon maaf, fitur pencarian masih dalam pengembangan","warning");
+	 return false;
+}); 
+
+$(document).on('click','.btn-detail',function(e){ 
+	var id = $(this).attr('data-id');
+	$.ajax({
+		method:"GET",
+		cache:false,
+		url:'home/detail/'+id
+	})
+	.done(function(view) {
+		$('#loading').hide();
+
+		$(".modal-title").html("Hai, salam kenal dari saya");
+		$(".modal-body").html(view);
+		$('#my-modal').modal('show'); 
+
+	})
+	.fail(function(res){
+		console.log('Error Response !');
+		console.log("responseText", res.responseText);
+	});
+
+	$(".modal-title").html("Hai, sala kenal dari saya");
+	$('#my-modal').modal('show'); 
+}); 
