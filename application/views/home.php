5e95d411c4de9f7cb152a043b0ac6e3bb0e2a03c
diszajatnika <diszaid@gmail.com>
2020-02-18 12:08:17 +0700

    menambahkan cdn font awesome

--- application/views/home.php
+++ application/views/home.php
@@ -9,6 +9,7 @@
 <link rel="stylesheet" href="<?php echo base_url('asset/plugins/bootstrap/css/bootstrap.min.css')?>">
 <link rel="stylesheet" href="<?php echo base_url('asset/plugins/font-awesome/css/all.css')?>">
 <link rel="stylesheet" href="<?php echo base_url('asset/css/custom.css')?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
