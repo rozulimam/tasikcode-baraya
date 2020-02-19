10dbcf5fa396f2240597b71190ab7ddf06d02694
hadian <hadian.online@gmail.com>
2020-02-19 09:31:00 +0700

    Home - Fix aria-labelledby same with modal ID

--- application/views/home.php
+++ application/views/home.php
@@ -100,7 +100,7 @@
     </footer>
 
 
-    <div class="modal fade" id="my-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="my-modal" tabindex="-1" role="dialog" aria-labelledby="my-modal" aria-hidden="true">
         <div class="modal-dialog modal-md modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
