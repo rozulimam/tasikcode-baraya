519cd547a60208f6bfa0671907ad6719c014516e
Muffin Latch <drones920@gmail.com>
2020-02-21 15:28:56 +0700

    animation loading search

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,3 +1,16 @@
+$(document).ajaxStart(function(){
+	$('.spinner').remove();
+	$('.not-found').remove();
+	$('.content').append(`<div class="spinner">
+	<div class="bounce1"></div>
+	<div class="bounce2"></div>
+	<div class="bounce3"></div>
+	</div>`);
+	setTimeout(() => {
+		$('.spinner').remove();
+	},600);
+});
+
 function searchMember() {
 	let keyword = $('#keyword').val();
 	if(keyword != ''){
@@ -10,31 +23,42 @@ function searchMember() {
 				$('.members').remove();
 				if(result.length < 1) {
 					$('.members').remove();
+					$('#keyword').val('');
+					$('main > div.py-5').css('min-height', '610px');
+					setTimeout(() => {
+						$('.content').append(`
+						<p class="not-found ml-4"><em>Data tidak ditemukan</em></p>`);
+					}, 600);
 				} else {
 					$('#keyword').val('');
-					$.each(result, function(i, data) {
-						$('.intro').after(`
-						<div class="col-lg-3 col-md-6 col-sm-12 members">
-							<div class="card mb-4 shadow-sm"> 
-								<img class='bd-placeholder-img card-img-top' src="asset/img/`+ data.link_img +`.png" onerror="this.src='asset/img/github-logo.png';"/>
-								<div class="card-body"> 
-									<p class="card-text">
-										<center>
-											<b>`+ data.name.toUpperCase()  +`</b><br>
-											<small class='text-black-50'>`+ data.title +`</small>
-										</center>
-									</p>
-									<div class="d-flex justify-content-between align-items-center"> 
-										<button class="btn btn-sm btn-outline-secondary btn-block btn-detail" data-id="`+ data.id +`">Kenali lebih dekat</button>  
+					$('main > div.py-5').removeAttr('style');
+					setTimeout(() => {
+						$.each(result, function(i, data) {
+							$('.content').append(`
+							<div class="col-lg-3 col-md-6 col-sm-12 members">
+								<div class="card mb-4 shadow-sm"> 
+									<img class='bd-placeholder-img card-img-top' src="asset/img/`+ data.link_img +`" onerror="this.src='asset/img/github-logo.png';"/>
+									<div class="card-body"> 
+										<p class="card-text">
+											<center>
+												<b>`+ data.name.toUpperCase()  +`</b><br>
+												<small class='text-black-50'>`+ data.title +`</small>
+											</center>
+										</p>
+										<div class="d-flex justify-content-between align-items-center"> 
+											<button class="btn btn-sm btn-outline-secondary btn-block btn-detail" data-id="`+ data.id +`">Kenali lebih dekat</button>  
+										</div>
 									</div>
 								</div>
-							</div>
-						</div> 
-						`);
-					});
+							</div> 
+							`);
+						});
+					}, 600);
 				}
 			}
 		});
+	} else {
+		window.location.reload();
 	}
 }
 
